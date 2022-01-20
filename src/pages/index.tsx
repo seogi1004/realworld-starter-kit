532dd65b623a2d79ef4799229008ea37f6e5de06
alvin.hhh <alvin@jennifersoft.com>
2022-01-20 21:58:46 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next';
 import type { PageProps } from '../types';
+import { useEffect } from 'react'
 import Layout from '../components/Layout';
 
 const Home: NextPage<PageProps> = ({ user }: PageProps) => {
+  useEffect(() => {
+    if (user !== null) {
+      fetch('/api/hello').then(res => {
+        console.log(res);
+      });
+    }
+  }, []);
+  
   return (
     <>
       <Layout user={user}>index.tsx</Layout>
